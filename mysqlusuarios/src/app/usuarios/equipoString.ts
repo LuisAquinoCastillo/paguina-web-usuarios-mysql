ae4042f769875e5220ccb693b84cd88003904660
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-05-03 16:49:22 -0500

    Jueves 3 de Maayo del 2018 Actualización 4:49pm

---
+++ mysqlusuarios/src/app/usuarios/equipoString.ts
@@ -0,0 +1,18 @@
+export class EquipoString {
+    idUsuario?: string;
+    nombre?: string;
+    apellidoPat?: string;
+    apellidoMat?: string;
+    fechaNac?: string;
+    curp?: string;
+    calle?: string;
+    numeroCasa?: string;
+    colonia?: string;
+    delegacion?: string;
+    estado?: string;
+    tipoPermiso?: string;
+    estatus?: string;
+    telefonoCas?: string;
+    telefonoCel?: string;
+}
+
---
+++ mysqlusuarios/src/app/usuarios/estatus.ts
@@ -0,0 +1,4 @@
+export class Estatus {
+    success?: boolean;
+    respuesta?: string;
+}
