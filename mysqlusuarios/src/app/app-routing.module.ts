37a85cb26e4bad270ce564b0a753bc2181436995
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-05-04 00:57:51 -0500

    Viernes 4 de Mayo del 2018 Actualización 12:57 am

--- mysqlusuarios/src/app/app-routing.module.ts
+++ mysqlusuarios/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/* importacion de componentes de las paginas*/
+import {UsuariosComponent} from './usuarios/usuarios.component';
+
+const routes: Routes = [
+  {path: 'usuarios', component: UsuariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ UsuariosComponent ];
--- mysqlusuarios/src/app/usuarios/usuarios.component.ts
+++ mysqlusuarios/src/app/usuarios/usuarios.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+/* Importacion de Http */
+import { HttpClient } from '@angular/common/http';
+/*Importacion de clases */
+import { Estatus } from './estatus';
+import { EquipoString } from './equipoString';
+
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
-export class UsuariosComponent implements OnInit {
+export class UsuariosComponent {
+  estatus: Estatus;
+  todosUsuarios: EquipoString[];
+  operaciones: EquipoString;
+  formulario: EquipoString = {
+    nombre: 'Luis',
+    apellidoPat: 'Aquino',
+    apellidoMat: 'Pérez',
+    fechaNac: '1996-11-15',
+    curp: 'AUPL670831',
+    calle: 'Hda. Tomacoco',
+    numeroCasa: '54',
+    colonia: 'Impulsora popular',
+    delegacion: 'Nezahualcoyotl',
+    estado: 'México',
+    tipoPermiso: 'Usuario',
+    estatus: 'true',
+    telefonoCas: '57830247',
+    telefonoCel: '5556987225'
+  };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    setTimeout(() => {
+      this.http.get<EquipoString[]>('localhost:8080/rest/Usuarios/todos').
+      subscribe(respuesta => {this.todosUsuarios = respuesta; });
+    }, 1200);
   }
 
+  // Metodo para guardar un usuario
+  guardarRegistro() {
+    this.http.post<Estatus>('localhost:8080/rest/Usuarios/guardar', this.formulario).
+    subscribe(respuesta => {this.estatus = respuesta; });
+    setTimeout(() => {}, 1500);
+  }
 }
